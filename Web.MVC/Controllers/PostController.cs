61d551f8021b6ba6367681d28700c3ef7c2caf05
ozeraydin57 <ozeraydin57@gmail.com>
2020-07-10 11:35:51 +0300

    menü yapısı oluşturuldu

---
+++ Web.MVC/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.MVC.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: PostController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PostController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
