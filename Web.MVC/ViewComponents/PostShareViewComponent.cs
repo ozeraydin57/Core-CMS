854ac1576d68590404645a50c2dcb5faa5f71ac1
ozeraydin57 <ozeraydin57@gmail.com>
2020-07-13 21:20:34 +0300

    share component eklendi

---
+++ Web.MVC/ViewComponents/PostShareViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.MVC.ViewComponents
+{
+    public class PostShareViewComponent : ViewComponent
+    {
+        public ViewViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
