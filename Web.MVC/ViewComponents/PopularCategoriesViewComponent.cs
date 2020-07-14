f8bb563da708aa1eca2264c392f3835bdc3cc9e1
ozeraydin57 <ozeraydin57@gmail.com>
2020-07-14 17:25:43 +0300

    layout düzenlemesi

---
+++ Web.MVC/ViewComponents/PopularCategoriesViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.MVC.ViewComponents
+{
+    public class PopularCategoriesViewComponent : ViewComponent
+    {
+        public ViewViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
