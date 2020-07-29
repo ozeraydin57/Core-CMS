a42f196f360b34760dc2654df902fc8a60fb8e7a
ozeraydin57 <ozeraydin57@gmail.com>
2020-07-29 09:45:36 +0300

    author componenti çalışması

---
+++ Web.MVC/ViewComponents/AuthorBoxViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tier.Business.Abstract;
+using Tier.Entities.Concrete;
+
+namespace Web.MVC.ViewComponents
+{
+    public class AuthorBoxViewComponent : ViewComponent
+    {
+
+        private IUserService _userService;
+        public AuthorBoxViewComponent(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public ViewViewComponentResult Invoke(int userId)
+        {
+            var model = new ResponseModel<User>
+            {
+                Success = true,
+                Data = _userService.GetById(userId)
+            };
+
+            return View(model);
+        }
+    }
+}
