057d1ffa8cbcbb7f3250a7a0e192501b3651d638
Özer Aydın <ozeraydin57@hotmail.com>
2021-01-27 16:30:48 +0300

    converted project framework .net 5

--- Core/DataAccess/EntitiyFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntitiyFramework/EfEntityRepositoryBase.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 
 namespace Core.DataAccess.EntitiyFramework
